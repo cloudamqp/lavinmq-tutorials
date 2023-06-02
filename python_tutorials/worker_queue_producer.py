b29068eabcaa88edd70b84fd0fede22903734c3b
Nyior <nyior.clement@aun.edu.ng>
2023-06-02 09:44:23 +0100

    Add work queue producer code

---
+++ python_tutorials/worker_queue_producer.py
@@ -0,0 +1,52 @@
+# producer.py
+
+import pika, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Access the CLOUDAMQP_URL environment variable and parse it (fallback to localhost)
+url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
+
+# Create a connection
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+print("[✅] Connection over channel established")
+
+channel = connection.channel() # start a channel
+channel.queue_declare(queue="image_resize_queue") # Declare a queue
+
+def send_to_queue(channel, routing_key, body):
+  channel.basic_publish(
+        exchange='',
+        routing_key=routing_key,
+        body=body
+  )
+  print(f"[📥] Message sent to queue #{body}")
+
+# Publish messages
+send_to_queue(
+    channel=channel, routing_key="image_resize_queue", body="Resize an image - 1"
+)
+send_to_queue(
+    channel=channel, routing_key="image_resize_queue", body="Resize an image - 2"
+)
+send_to_queue(
+    channel=channel, routing_key="image_resize_queue", body="Resize an image - 3"
+)
+send_to_queue(
+    channel=channel, routing_key="image_resize_queue", body="Resize an image - 4"
+)
+send_to_queue(
+    channel=channel, routing_key="image_resize_queue", body="Resize an image - 5"
+)
+send_to_queue(
+    channel=channel, routing_key="image_resize_queue", body="Resize an image - 6"
+)
+
+try:
+  connection.close()
+  print("[❎] Connection closed")
+except Exception as e:
+  print(f"Error: #{e}")
+  
