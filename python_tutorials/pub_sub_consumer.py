112a9f2ac2d7d7b3f0baaa9a027cca501f6da791
Nyior <nyior.clement@aun.edu.ng>
2023-06-04 14:57:51 +0100

    Add pub sub consumer

--- python_tutorials/pub_sub_consumer.py
+++ python_tutorials/pub_sub_consumer.py
@@ -1,4 +1,4 @@
-import pika, os, sys, time
+import pika, os, sys
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,22 +13,47 @@ print("[✅] Connection over channel established")
 
 channel = connection.channel() # start a channel
 
+user_groups = ["hr", "marketing", "support"]
+
+# Get user group argument from commandline
+user_group = sys.argv[1]
+if not user_group:
+    sys.stderr.write("Usage: %s [hr] [marketing] [support]\n" % sys.argv[0])
+    sys.exit(1)
+
+if user_group not in user_groups:
+    sys.stderr.write("Invalid argument - allowed arguments: %s [hr] [marketing] [support]\n" % sys.argv[0])
+    sys.exit(1)
+
+queue_name = user_group + "_queue"
+queue_binding_key = user_group
+exchange_name = "slack_notifications"
+
+channel.exchange_declare(
+  exchange=exchange_name,
+  exchange_type='fanout'
+) # Declare an exchange
+
 channel.queue_declare(
-  queue="image_resize_queue_1",
+  queue=queue_name,
   durable=True
 ) # Declare a queue
 
+# Create a binding
+channel.queue_bind(
+  exchange=exchange_name,
+  queue=queue_name,
+  routing_key=queue_binding_key
+)
+
 def callback(ch, method, properties, body):
     print(f"[✅] Received #{ body }")
-    time.sleep(5)
-    print("[✅] Image resized!")
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
 channel.basic_consume(
-    "image_resize_queue_1",
+    queue_name,
     callback,
 )
-
 
 try:
   print("\n[❎] Waiting for messages. To exit press CTRL+C \n")
