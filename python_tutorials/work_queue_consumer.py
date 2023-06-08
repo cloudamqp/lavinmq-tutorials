142f75dd9432d701d76268437f6e8930d0bee3f3
Nyior <nyior.clement@aun.edu.ng>
2023-06-08 13:59:52 +0100

    Update task queues code sample

--- python_tutorials/work_queue_consumer.py
+++ python_tutorials/work_queue_consumer.py
@@ -4,28 +4,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Access the CLOUDAMQP_URL environment variable and parse it (fallback to localhost)
-url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
+url = os.environ.get(
+  'CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f'
+)
 
-# Create a connection
 params = pika.URLParameters(url)
 connection = pika.BlockingConnection(params)
 print("[✅] Connection over channel established")
 
-channel = connection.channel() # start a channel
+channel = connection.channel()
 channel.queue_declare(
-  queue="image_resize_queue_1",
-  durable=True
-) # Declare a queue
+  queue="image_resize_queue_1", durable=True
+)
 
 def callback(ch, method, properties, body):
     print(f"[✅] Received #{ body }")
-    time.sleep(5)
+    time.sleep(5) # Some time consuming task
     print("[✅] Image resized!")
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(
     "image_resize_queue_1",
     callback,
+    auto_ack=False
 )
 
 try:
